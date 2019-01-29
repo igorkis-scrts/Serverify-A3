3ebf5046a8874bf76f3ff31026d10e1d14ae0e43
Igor Kiselyov <igorkiselyov74@live.com>
2019-01-29 21:57:24 +0500

    First implementation of CurrentProfile singleton, creators update hamburger menu style, current profile label in window header

---
+++ A3ServerTool/Models/CurrentProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3ServerTool.Models
+{
+    /// <summary>
+    /// Current selected profile to perform all our operations
+    /// </summary>
+    public sealed class CurrentProfile
+    {
+        // ReSharper disable once InconsistentNaming
+        private static readonly CurrentProfile _instance = new CurrentProfile();
+
+        static CurrentProfile() {}
+
+        private CurrentProfile() {}
+
+        // ReSharper disable once ConvertToAutoProperty
+        public static CurrentProfile Instance => _instance;
+    }
+}
