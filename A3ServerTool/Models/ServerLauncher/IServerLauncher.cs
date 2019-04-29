e21d2fa13a2d453ec194d99f3f1ed077971bd6c1
Igor Kiselyov <igorkiselyov74@live.com>
2019-04-29 16:03:18 +0500

    Changed folder structure in models folder

---
+++ A3ServerTool/Models/ServerLauncher/IServerLauncher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3ServerTool.Models
+{
+    /// <summary>
+    /// Provides instruments to launch server with all it's settings.
+    /// </summary>
+    public interface IServerLauncher
+    {
+        /// <summary>
+        /// Runs server.
+        /// </summary>
+        /// <param name="profile">Server profile.</param>
+        void Run(Profile profile);
+    }
+}
---
+++ A3ServerTool/Models/ServerLauncher/ServerLauncher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3ServerTool.Models
+{
+    /// <inheritdoc/>
+    public class ServerLauncher : IServerLauncher
+    {
+        /// <inheritdoc/>
+        public void Run(Profile profile)
+        {
+            if (profile == null) return;
+
+            //TODO: grab cmd arguments
+            //TODO: grab basic.cfg path
+            //TODO: grab server.cfg path
+            //TODO: grab profilePath
+
+            var server = new System.Diagnostics.Process
+            {
+                StartInfo =
+                {
+                    FileName = profile.ArgumentSettings.ExecutablePath,
+                    Arguments = AppendProfileSettings(profile),
+                    UseShellExecute = false,
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Maximized
+                }
+            };
+            server.Start();
+        }
+
+        private string AppendProfileSettings(Profile profile)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("-cfg=" + profile.BasicConfigString);
+
+            var tt = builder.ToString();
+
+            return tt;
+        }
+    }
+}
