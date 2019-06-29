69d46f5b4bc62fdd92e10a06ae376b94f657a947
Igor Kiselyov <igorkiselyov74@live.com>
2019-06-29 18:12:04 +0500

    Rename of arma profile

--- A3ServerTool/Models/Config/ArmaProfile.cs
+++ A3ServerTool/Models/Config/ArmaProfile.cs
@@ -6,140 +6,185 @@ namespace A3ServerTool.Models.Config
     /// <summary>
     /// Represents server difficulty settings.
     /// </summary>
-    public class Arma3Profile : IConfig
+    public class ArmaProfile : IConfig
     {
         public string FileLocation { get; set ; }
+
+        /// <summary>
+        /// Gets or sets the overall difficulty for the server.
+        /// </summary>
+        [ConfigProperty(PropertyName = "difficulty")]
+        public DifficultyType Difficulty { get; set; }
 
         /// <summary>
         /// Gets or sets the value if damage will be decreased for players and AI members of his group.
         /// </summary>
         [ConfigProperty(PropertyName = "reducedDamage")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int ReducedDamage { get; set; }
 
         /// <summary>
         /// Gets or sets the value if indication icons will be shown on units in player's group.
         /// </summary>
         [ConfigProperty(PropertyName = "groupIndicators")]
-        public ThreeStateType GroupIndicationType { get; set; }
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
+        public int GroupIndicationType { get; set; }
 
         /// <summary>
         /// Gets or sets the value if friendly unit tags will be shown.
         /// </summary>
         [ConfigProperty(PropertyName = "friendlyTags")]
-        public ThreeStateType FriendlyTagsVisibilityType { get; set; }
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
+        public int FriendlyTagsVisibilityType { get; set; }
 
         /// <summary>
         /// Gets or sets the value if enemy unit tags will be shown.
         /// </summary>
         [ConfigProperty(PropertyName = "enemyTags")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int AreEnemyTagsShown { get; set; }
 
         /// <summary>
         /// Gets or sets the value if mine positions will be shown.
         /// </summary>
         [ConfigProperty(PropertyName = "detectedMines")]
-        public ThreeStateType AreDetectedMinesShown { get; set; }
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
+        public int AreDetectedMinesShown { get; set; }
 
         /// <summary>
         /// Gets or sets the value for showing commands to player.
         /// </summary>
         [ConfigProperty(PropertyName = "commands")]
-        public ThreeStateType CommandsVisibilityType { get; set; }
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
+        public int CommandsVisibilityType { get; set; }
 
         /// <summary>
         /// Gets or sets the value for showing waypoints for player.
         /// </summary>
         [ConfigProperty(PropertyName = "waypoints")]
-        public ThreeStateType WaypointsVisibilityType { get; set; }
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
+        public int WaypointsVisibilityType { get; set; }
 
         /// <summary>
         /// Gets or sets the value for showing weapon info for player.
         /// </summary>
         [ConfigProperty(PropertyName = "weaponInfo")]
-        public ThreeStateType WeaponInfoVisibilityType { get; set; }
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
+        public int WeaponInfoVisibilityType { get; set; }
 
         /// <summary>
         /// Gets or sets the value for showing player's stance.
         /// </summary>
         [ConfigProperty(PropertyName = "stanceIndicator")]
-        public ThreeStateType StanceIndicatorVisibilityType { get; set; }
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
+        public int StanceIndicatorVisibilityType { get; set; }
 
         /// <summary>
         /// Gets or sets the value if stamina bar will be shown to player.
         /// </summary>
         [ConfigProperty(PropertyName = "staminaBar")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsStaminaBarShown { get; set; }
 
         /// <summary>
         /// Gets or sets the value if crosshair will be shown to player.
         /// </summary>
         [ConfigProperty(PropertyName = "weaponCrosshair")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsCrosshairShown { get; set; }
 
         /// <summary>
         /// Gets or sets the value if vision aid markers will be shown to player.
         /// </summary>
         [ConfigProperty(PropertyName = "visionAid")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsVisionAidAllowed { get; set; }
 
         /// <summary>
         /// Gets or sets the value for allowing third person view.
         /// </summary>
         [ConfigProperty(PropertyName = "thirdPersonView")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsThirdPersonAllowed { get; set; }
 
         /// <summary>
         /// Gets or sets the value for allowing camera shake.
         /// </summary>
         [ConfigProperty(PropertyName = "cameraShake")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsCameraShakeAllowed { get; set; }
 
         /// <summary>
         /// Gets or sets the value for displaying table with kills, deaths and overall score in multiplayer.
         /// </summary>
         [ConfigProperty(PropertyName = "scoreTable")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsScoreTableShown { get; set; }
 
         /// <summary>
         /// Gets or sets the value for showing death messages in chat window.
         /// </summary>
         [ConfigProperty(PropertyName = "deathMessages")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int AreDeathMessagesShown { get; set; }
 
         /// <summary>
         /// Gets or sets the value for showing who speaks through VON communication.
         /// </summary>
         [ConfigProperty(PropertyName = "vonID")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int AreVonIdsShown { get; set; }
 
         /// <summary>
         /// Gets or sets the value for showing friendly units, enemy units and detected mines on the map.
         /// </summary>
         [ConfigProperty(PropertyName = "mapContent")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsExtendedMapContentAllowed { get; set; }
 
         /// <summary>
         /// Gets or sets the value for enabling/disabling automatic reporting of spotted enemies by players only.
         /// </summary>
         [ConfigProperty(PropertyName = "autoReport")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsAutoReportEnabled { get; set; }
 
         /// <summary>
         /// Gets or sets the value for enabling/disabling multiple saves in a mission.
         /// </summary>
         [ConfigProperty(PropertyName = "multipleSaves")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int AreMultipleSavesAllowed { get; set; }
 
         /// <summary>
         /// Gets or sets the value for allowing tactical ping for players.
         /// </summary>
         [ConfigProperty(PropertyName = "tacticalPing")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "Options" })]
         public int IsTacticalPingAllowed { get; set; }
 
         /// <summary>
-        /// Gets or sets the ai level.
+        /// Gets or sets the ai level preset.
         /// </summary>
-        [ConfigProperty(IgnoreParsing =true)]
-        public AiLevel AiLevel { get; set; }
+        /// <value>
+        /// The ai level preset.
+        /// </value>
+        [ConfigProperty(PropertyName = "aiLevelPreset", IsIntValueRequired = true)]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty"})]
+        public AiLevelPresetType AiLevelPreset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ai skill.
+        /// </summary>
+        [ConfigProperty(PropertyName = "skillAI")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "CustomAiLevel" })]
+        public float AiSkill { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ai precision.
+        /// </summary>
+        [ConfigProperty(PropertyName = "precisionAI")]
+        [WrappingClass(new string[] { "DifficultyPresets", "CustomDifficulty", "CustomAiLevel" })]
+        public float AiPrecision { get; set; }
     }
 }
