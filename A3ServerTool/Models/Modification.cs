4ab2762440a3db0cbad10f37eb0445cb94744b67
Igor Kiselyov <igorkiselyov74@live.com>
2019-07-07 13:03:19 +0500

    hide slow network rules - related controls, modifications menu mockup

---
+++ A3ServerTool/Models/Modification.cs
@@ -0,0 +1,23 @@
+﻿namespace A3ServerTool.Models
+{
+    /// <summary>
+    /// Represents game mod.
+    /// </summary>
+    public class Modification
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is selected.
+        /// </summary>
+        public bool IsClientMod { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is whitelisted.
+        /// </summary>
+        public bool IsServerMod { get; set; }
+    }
+}
