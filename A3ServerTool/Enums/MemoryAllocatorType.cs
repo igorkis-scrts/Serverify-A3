7c44dc3de76b728318629ea4f7fb8a542caff8e8
Igor Kiselyov <igorkiselyov74@live.com>
2019-07-17 21:38:04 +0500

    memory allocator enum

---
+++ A3ServerTool/Enums/MemoryAllocatorType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace A3ServerTool.Enums
+{
+    /// <summary>
+    /// Provides enum for various memory allocators for A3.
+    /// </summary>
+    public enum MemoryAllocatorType
+    {
+        [Description("Not Specified")]
+        NotSpecified = 0,
+
+        [Description("TBB4_BI")]
+        tbb4malloc_bi = 1,
+
+        [Description("JEMalloc_BI")]
+        jemalloc_bi = 2,
+
+        [Description("System")]
+        system = 3
+    }
+}
