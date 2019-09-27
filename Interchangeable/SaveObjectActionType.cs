c5dad53864efb246fa0197b97fb1aef1ece772fa
Igor Kiselyov <igorkiselyov74@live.com>
2019-09-27 14:37:16 +0500

    DialogResult refacturing, add objectactiontype enum

---
+++ Interchangeable/SaveObjectActionType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interchangeable
+{
+    /// <summary>
+    /// Represents saved object action context enum values.
+    /// </summary>
+    public enum SaveObjectActionType
+    {
+        None = 0,
+        Update = 1,
+        Create = 2
+    }
+}
